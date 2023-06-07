c7f56cc9318b0aaa94cc00ed136cf69d2319a02f
ntruter42 <ntruter42@gmail.com>
2023-06-07 03:38:58 +0200

    Test addPrize, removePrize, setPrizes, getPrizes

--- unit-testing/qrush.test.js
+++ unit-testing/qrush.test.js
@@ -1,63 +1,105 @@
-describe('The QRush App factory function', function() {
-    let qRush;
-    beforeEach(function(){
-        qRush = QRushFactory();
-    });
+describe('The QRush App factory function', function () {
+	let qRush;
 
-    describe('set and get functionality', function() {
-     
+	beforeEach(function () {
+		qRush = QRushFactory();
+	});
 
-        it('should be able to set and get the treasure prize', function() {
-            qRush.setKnownChests([{}, {}]);
-            assert.deepEqual([{}, {}], qRush.getKnownChests());
-        });
-    
-        it('should be able to set and get the treasure prize', function() {
-            qRush.setKnownChests([{ gshvfjsf : 214251, ugfjnsmf : '34634huvbjkds' }, { hjmdnbfs : 3275312 }]);
-            assert.deepEqual([{ gshvfjsf : 214251, ugfjnsmf : '34634huvbjkds' }, { hjmdnbfs : 3275312 }], qRush.getKnownChests());
-        });
+	describe('setKnownChests, getKnownChests', function () {
+		it('should be able to set and get the treasure chests', function () {
+			let localStorageChest = [];
 
-        it('should be able to initialise the known chest in the factory function', function() {
-            qRush = QRushFactory([ { kydvhf : 12431521, bvnfjffg : 'kjbrgrgr' } ]);
-            assert.deepEqual([ { kydvhf : 12431521, bvnfjffg : 'kjbrgrgr' } ], qRush.getKnownChests());
-        });
+			qRush.setKnownChests(localStorageChest);
+			assert.deepEqual([], qRush.getKnownChests());
+		});
 
-    });
-    
+		it('should be able to set and get the treasure chests', function () {
+			let localStorageChest = [{ chest_id: 21425108, chest_location: '-33.92804417019321, 18.423937019115005' }, { chest_id: 32753124, chest_location: '-33.927047504280935, 18.45548536441777' }];
 
+			qRush.setKnownChests(localStorageChest);
+			assert.deepEqual(localStorageChest, qRush.getKnownChests());
+		});
 
+		it('should be able to initialise the known chests in the factory function', function () {
+			let localStorageChest = [{ chest_id: 53903426, chest_location: '-33.91786995338542, 18.422654047132987' }, { chest_id: 24982562, chest_location: '-33.92938377623288, 18.41104307597263' }];
 
-    describe('Creating a sponsor Form.' , function(){
-   
+			qRush.setKnownChests(localStorageChest);
+			assert.deepEqual(localStorageChest, qRush.getKnownChests());
+		});
+	});
 
-        it('It should return a registered company named Shoprite.' , function(){
-             
-            let CompanyName = {"companyName":'Shoprite', "branchAddress":'Ottery', "Date":'2023/06/6', "Email":'shoprite@gmail.com', "extrasDetails" : "0"};
-            
-            qRush.addSponsor(CompanyName)
-            assert.deepEqual([CompanyName], qRush.getSponsors());
-        })
-    
-        
-        it('It should return a registered company named Woolworths.' , function(){
-             
-            let CompanyName = {"companyName":'Woolworths', "branchAddress":'Claremont', "Date":'2023/05/6', "Email":'woolies@gmail.com', "extrasDetails" : "0"};
-           
-            qRush.addSponsor(CompanyName)
-            assert.deepEqual([CompanyName], qRush.getSponsors());
-        })
-    
-        it('It should return an error message, 1 or more sections have not been been filled in.' , function(){
-             
-            let CompanyName = {"companyName":'', "branchAddress":'Claremont', "Date":'2023/05/6', "Email":'', "extrasDetails" : "0"};
-            qRush.addSponsor(CompanyName)
-            assert.deepEqual([], qRush.getSponsors());
+	describe('addSponsor, getSponsor', function () {
+		it('should return a registered company named Shoprite.', function () {
+			let CompanyName = { "companyName": 'Shoprite', "branchAddress": 'Ottery', "Date": '2023/06/6', "Email": 'shoprite@gmail.com' };
 
+			qRush.addSponsor(CompanyName);
+			assert.deepEqual([CompanyName], qRush.getSponsors());
+		})
 
-        })
-    
-       
-    })
+		it('should return a registered company named Woolworths.', function () {
+			let CompanyName = { "companyName": 'Woolworths', "branchAddress": 'Claremont', "Date": '2023/05/6', "Email": 'woolies@gmail.com' };
 
+			qRush.addSponsor(CompanyName);
+			assert.deepEqual([CompanyName], qRush.getSponsors());
+		})
 
+		it('should return an error message, 1 or more sections have not been been filled in.', function () {
+			let CompanyName = { "companyName": '', "branchAddress": 'Claremont', "Date": '2023/05/6', "Email": '' };
+
+			qRush.addSponsor(CompanyName);
+			assert.deepEqual([], qRush.getSponsors());
+		})
+	});
+
+	describe('addPrize, removePrize, setPrizes, getPrizes', function () {
+		it('should add a prize to list of prizes', function () {
+			let newPrize = { 'prize_id': 34564565, 'prize_title': 'R200 Uber Voucher', 'sponsor_name': 'Uber' };
+
+			qRush.addPrize(newPrize);
+			assert.equal([newPrize], qRush.getPrizes());
+		});
+
+		it('should add multiple prizes to list of prizes', function () {
+			let newPrize1 = { 'prize_id': 34564565, 'prize_title': 'R200 Uber Voucher', 'sponsor_name': 'Uber' };
+			let newPrize2 = { 'prize_id': 34564566, 'prize_title': '5GB Telkom Data Bundle', 'sponsor_name': 'Telkom' };
+
+			qRush.addPrize(newPrize1);
+			qRush.addPrize(newPrize2);
+
+			assert.equal([newPrize1, newPrize2], qRush.getPrizes());
+		});
+
+		it('should remove a prize from list of prizes', function () {
+			let newPrize1 = { 'prize_id': 34564565, 'prize_title': 'R200 Uber Voucher', 'sponsor_name': 'Uber' };
+			let newPrize2 = { 'prize_id': 34564566, 'prize_title': '5GB Telkom Data Bundle', 'sponsor_name': 'Telkom' };
+
+			qRush.addPrize(newPrize1);
+			qRush.addPrize(newPrize2);
+
+			assert.equal([newPrize1, newPrize2], qRush.getPrizes());
+
+			qRush.removePrize(newPrize1);
+
+			assert.equal([newPrize2], qRush.getPrizes());
+		});
+
+		it('should set a list of prizes', function () {
+			let newPrize1 = { 'prize_id': 34564565, 'prize_title': 'R200 Uber Voucher', 'sponsor_name': 'Uber' };
+			let newPrize2 = { 'prize_id': 34564566, 'prize_title': '5GB Telkom Data Bundle', 'sponsor_name': 'Telkom' };
+
+			qRush.addPrize(newPrize1);
+			qRush.addPrize(newPrize2);
+
+			let prizeList = qRush.getPrizes();
+
+			qRush.removePrize(newPrize1);
+			qRush.removePrize(newPrize2);
+
+			assert.equal([], qRush.getPrizes());
+
+			qRush.setPrizes(prizeList);
+
+			assert.equal([newPrize1, newPrize2], qRush.getPrizes());
+		});
+	});
 });
