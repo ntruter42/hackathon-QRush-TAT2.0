1ee331cc26a1d92800e7f1633b3ee53077dba887
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-06-08 19:12:24 +0200

    removed line 12 to line 17 in the scan.dom.js file

--- js/scan.dom.js
+++ js/scan.dom.js
@@ -9,12 +9,6 @@ function onScanSuccess(decodedText, decodedResult) {
 	if (!isNaN(decodedText)) {
 		if (Number(decodedText) >= 1000 && Number(decodedText) < 2000) {
 			qRushScan.discoverChest(Number(decodedText));
-			// create an element for the pop up message
-			const treasureFoundElement = document.createElement('div');
-			// setting the innerText of the treasureFoundElement
-			treasureFoundElement.innerText = 'Treasure found!';
-			// append the newly created element to the dom
-			document.body.append(treasureFoundElement);
 		}
 	}
 }
