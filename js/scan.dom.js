37370f47d9e7d1aacb6d195fbd83d7967bb239cd
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-06-08 22:50:24 +0200

    used the factory function methods to get the chest object, get prize object and used document.getElementById() to get the elements reference

--- js/scan.dom.js
+++ js/scan.dom.js
@@ -1,3 +1,10 @@
+// elements reference
+
+const foundTreasure = document.querySelector('.treasureFound');
+
+// hide the foundTreasure element for the first time
+foundTreasure.style.display = 'none';
+
 const qRushScan = QRushFactory();
 
 const collectButton = document.querySelector('button');
@@ -7,14 +14,24 @@ function onScanSuccess(decodedText, decodedResult) {
 	// console.log(`Scan result: ${decodedText}`, decodedResult);
 
 	if (!isNaN(decodedText)) {
-		if (Number(decodedText) >= 1000 && Number(decodedText) < 2000) {
-			qRushScan.discoverChest(Number(decodedText));
+		let chest_id = Number(decodedText);
+
+		if (chest_id >= 1000 && chest_id < 2000) {
+			qRushScan.discoverChest(chest_id);
+			let foundChest = qRushScan.getChestObject(chest_id);
+			let foundPrize = qRushScan.getPrizeObject(foundChest.prize_id);
+
+			document.getElementById("prize-img").src = foundPrize.url;
+			document.getElementById("prize-img").classList.remove('hidden');
+			document.getElementById("reader").classList.add('hidden');
+			// when the scan is successful the foundTreasure element will appear again
+			foundTreasure.style.display = 'block';
 		}
 	}
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
-	"reader", { fps: 1, qrbox: 400 }
+	"reader", { fps: 50, qrbox: 400 }
 );
 
 html5QrcodeScanner.render(onScanSuccess);
