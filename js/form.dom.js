0890890b7eb8240f2c594f1739dd17247f93c38a
ntruter42 <ntruter42@gmail.com>
2023-06-10 21:22:53 +0200

    Minor changes to form

--- js/form.dom.js
+++ js/form.dom.js
@@ -5,6 +5,7 @@ const lastName = document.querySelector(".lastName");
 const emailAddress = document.querySelector(".email");
 const branchAddress = document.querySelector(".branchAddress");
 const rewardTitle = document.querySelector(".rewardTitle");
+const rewardCount = document.querySelector("#rewardCount");
 const submitBtn = document.querySelector(".submit-button");
 
 // factory function instance
@@ -21,9 +22,7 @@ function getDetails() {
 		if (status == google.maps.GeocoderStatus.OK) {
 			let latitude = results[0].geometry.location.lat();
 			let longitude = results[0].geometry.location.lng();
-			console.log(qRushForm.getPrizes());
-			qRushForm.addPrize(companyName.value, fullName, emailAddress.value, latitude + ',' + longitude, rewardTitle.value);
-			console.log(qRushForm.getPrizes());
+			qRushForm.addPrize(companyName.value, fullName, emailAddress.value, latitude + ',' + longitude, rewardTitle.value, rewardCount);
 		}
 	});
 }
